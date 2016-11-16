6eb8d0d2d0fe5b52b1a8f9b85147456f7170672b
Ryan Dandrow <ryandandrow@gmail.com>
2016-11-15 23:42:13 -0600

    Fix broken fonts for development

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ module QuickCharts
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.paths << "#{Rails}/app/assets/fonts"
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-    config.assets.precompile += %w(.svg .eot .woff .ttf .otf)
+    # config.assets.paths << "#{Rails}/app/assets/fonts"
+    # config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+    # config.assets.precompile += %w(.svg .eot .woff .ttf .otf)
   end
 end
